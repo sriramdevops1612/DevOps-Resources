49e7b0b7006a97cd1d552fd46e016fd2cba8c419
sri ram kavali <sriramdevops1612@gmail.com>
2022-10-20 18:52:38 -0500

    sample project

---
+++ src/main/java/com/example/sampleproject/App.java
@@ -0,0 +1,13 @@
+package com.example.sampleproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
